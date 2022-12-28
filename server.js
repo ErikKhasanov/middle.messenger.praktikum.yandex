c25ea6be23cd29930a949b94e3d09e44b6147ac2
erik <erikkhasanov@yandex.ru>
2022-12-28 14:16:50 +0300

    add _redirects

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 server.use(express.static("./dist"));
 
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 server.listen(PORT, (req, res) => {
